a50f408e62e8717a94d81ffd18d8e245e390d195
Arthur Bowers <arthurbowers@gmail.com>
2020-02-28 19:41:55 +0000

    Adding player class.

--- player.cpp
+++ player.cpp
@@ -1,1 +1,106 @@
 #include "player.h"
+#include "inventory.h"
+
+// Creating the player.
+player::player(std::string name, int startStr, int startAgi, int startInt):
+    name(name),
+    strength(startStr),
+    agility(startAgi),
+    intelligence(startInt)
+{
+    lvl = 0;
+    xp = 0;
+    health = strength * 3;
+    hitChance = 0.5;
+}
+
+int player::heal() {
+    std::cout << "\n I'm healing, watch my back!" << std::endl;
+    return (intelligence / 2) + 3;
+}
+
+int player::gainXP() {
+    return (lvl /10);
+}
+
+std::string player::getName() {
+    return name;
+}
+
+int player::getHP() {
+    return health;
+}
+
+int player::getInt() {
+    return intelligence;
+}
+
+int player::getAgi() {
+    return agility;
+}
+
+int player::getStr() {
+    return strength;
+}
+
+int player::getLvl() {
+    return lvl;
+}
+
+int player::getDef() {
+    return def;
+}
+
+int player::getInvSize() {
+    return playersInv->getInvSize();
+}
+
+void player::createInv(itemTable* itemsTable, int invSize) {
+    playersInv = new inventory(itemsTable, invSize);
+}
+
+void player::setClass(int classChoice) {
+    switch (classChoice) {
+        case 1:
+            {
+                class = "Warrior";
+                classID = 1;
+            }
+            break;
+
+        case 2:
+            {
+                class = "Ranger";
+                classID = 2;
+            }
+            break;
+
+        case 3:
+            {
+                class = "Wizard";
+                classID = 3;
+            }
+            break;
+    }
+}
+
+void player::setDef(int classChoice) {
+    switch (classChoice) {
+        case 1:
+            {
+                def = player::getAgi() / 2;
+            }
+            break;
+
+        case 2:
+            {
+                def = player::getAgi() / 2;
+            }
+            break;
+        case 3:
+            {
+                def = 2;
+            }
+            break;
+    }
+}
--- player.h
+++ player.h
@@ -2,3 +2,46 @@
 
 #include <iostream>
 #include "inventory.h"
+
+// Check inventory.h for comments. Same.
+
+class player {
+    private:
+        std::string clss;
+        int health;
+        int intelligence;
+        int strength;
+        int agility;
+        int lvl;
+        int xp;
+        std::string name;
+        int def;
+        int classID;
+        float hitChance;
+
+    public:
+        player();
+        player(std::string name, int startStr, int startAgi, int startInt);
+
+        int heal();
+        int gainXP();
+        int getHP();
+        std::string getName();
+        int getInt();
+        int getAgi();
+        int getStr();
+        int getLvl();
+        int getXP();
+        int getDef();
+
+        // This need to be looked at carefully and questioned.
+
+        inventory* playersInv;
+        void createInv(itemTable* itemsTable, int invSize);
+        int getInvSize();
+        void setClass(int classChoice);
+        void setDef(int classChoice);
+        int getClassID();
+        float getHit();
+        void setHit(int classChoice);
+};
